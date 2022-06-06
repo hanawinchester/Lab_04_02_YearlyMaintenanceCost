2bbd10d0985a65af8c5569f2a1c67e6f2dd79c52
hanawinchester <hana.rose.winchester@gmail.com>
2022-06-05 20:39:17 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double spring = 5550.21;
+        double summer = 100.50;
+        double fall = 1099.90;
+        double winter = 201.45;
+        double total = spring + summer + fall + winter;
+        System.out.println("Spring Cost: $" + spring);
+        System.out.println("Summer Cost : $" + summer);
+        System.out.println("Fall Cost : $" + fall);
+        System.out.println("Winter Cost : $" + winter);
+
+        System.out.println("Total yearly maintenance cost: $" + total);
+    }
+}
